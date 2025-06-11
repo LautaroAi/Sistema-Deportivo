e942f783f08e18df4bd938fcacf9cbc60747eb29
Lautaro Rivieri <119972411+LautaroAi@users.noreply.github.com>
2025-06-11 22:53:06 +0000

    importando sql

---
+++ Sistema-Deportivo.py
@@ -0,0 +1,26 @@
+import sqlite3
+from tabulate import tabulate
+
+# Conexión y creación de tablas
+conn = sqlite3.connect(':memory:')
+cursor = conn.cursor()
+
+cursor.execute('''
+CREATE TABLE jugadores (
+id INTEGER PRIMARY KEY AUTOINCREMENT,
+nombre TEXT NOT NULL,
+edad INTEGER NOT NULL,
+posicion TEXT NOT NULL
+)
+''')
+
+cursor.execute('''
+CREATE TABLE partidos (
+id INTEGER PRIMARY KEY AUTOINCREMENT,
+fecha TEXT NOT NULL,
+rival TEXT NOT NULL,
+resultado TEXT CHECK(resultado IN ('Ganado', 'Perdido', 'Empatado')) NOT NULL
+)
+''')
+
+# Funciones
